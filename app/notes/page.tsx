f1e4f7dd628a0e08f7f0ae4c39e1d28903d2715e
Oleh Oprysk <opryskoleh2006@gmail.com>
2025-11-07 01:33:44 +0200

    fixed code

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,4 +1,3 @@
-// app/notes/page.tsx
 import { QueryClient, dehydrate } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
@@ -6,7 +5,6 @@ import NotesClient from "./Notes.client";
 export default async function NotesPage() {
   const queryClient = new QueryClient();
 
-  // Використовуємо об'єктну форму prefetchQuery і явно передаємо queryKey як масив
   await queryClient.prefetchQuery({
     queryKey: ["notes", "", 1],
     queryFn: () => fetchNotes("", 1),
